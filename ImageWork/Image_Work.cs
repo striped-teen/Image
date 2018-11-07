89600d1dd2a7f0aa7d046549a038f5f0bac43f05
Лейла Сайондзи <sherlocked1105@gmail.com>
2018-11-07 12:59:33 +0400

    Все действия с изображением

---
+++ ImageWork/Image_Work.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ImageWork
+{
+    class Image_Work
+    {
+        //для получения изображения из байтового массива
+        public static BitmapImage byteToImg(byte[] array)
+        {
+            var i = new BitmapImage();
+            using (var ms = new MemoryStream(array))
+            {
+                ms.Position = 0;
+                i.BeginInit();
+                i.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                i.CacheOption = BitmapCacheOption.OnLoad;
+                i.StreamSource = ms;
+                i.EndInit();
+            }
+            i.Freeze();
+            return i;
+        }
+
+        //для получения байтового массива из изображения
+        public static byte[] ImgToByte(string way)
+        {
+            byte[] ImgData = null;
+            try
+            {
+                FileStream stream = new FileStream(way, FileMode.Open, FileAccess.Read);
+                StreamReader read = new StreamReader(stream);
+                ImgData = new byte[stream.Length - 1];
+                stream.Read(ImgData, 0, (int)stream.Length - 1);
+                return ImgData;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return ImgData;
+        }
+
+        //для работыи изображения с мышью
+        public static void INK(FrameworkElement el, Canvas can, IInputElement t)
+        {
+
+            //для зума и вращения
+            el.MouseWheel += (ss, ee) =>
+            {
+                Matrix mat = el.RenderTransform.Value;
+                System.Windows.Point mouse = ee.GetPosition(el);
+                if (ee.RightButton == MouseButtonState.Pressed)
+                {
+                    if (ee.Delta > 0)
+                    {
+                        mat.RotateAtPrepend(10, mouse.X, mouse.Y);
+                    }
+                    else
+                    {
+                        mat.RotateAtPrepend(-10, mouse.X, mouse.Y);
+                    }
+                }
+                else
+                {
+                    if (ee.Delta > 0)
+                    {
+                        mat.ScaleAtPrepend(1.15, 1.15, mouse.X, mouse.Y);
+                    }
+                    else
+                    {
+                        mat.ScaleAtPrepend(1 / 1.15, 1 / 1.15, mouse.X, mouse.Y);
+
+                    }
+                }
+                MatrixTransform mtf = new MatrixTransform(mat);
+                el.RenderTransform = mtf;
+            };
+            System.Windows.Point first = new System.Windows.Point();
+
+            //для удаления двойным щелчком
+            el.MouseLeftButtonDown += (ss, ee) =>
+            {
+                first = ee.GetPosition(t);
+                el.CaptureMouse();
+                if (ee.ClickCount == 2)
+                {
+                    can.Children.Remove(el);
+                }
+            };
+
+            //для отпускания мыши
+            el.MouseUp += (ss, ee) =>
+            {
+                el.ReleaseMouseCapture();
+            };
+
+            //перемещение
+            el.MouseLeftButtonDown += (ss, ee) => {
+                first = ee.GetPosition(t);
+                el.CaptureMouse();
+            };
+
+            el.MouseMove += (ss, ee) =>
+            {
+                if (ee.LeftButton == MouseButtonState.Pressed)
+                {
+                    System.Windows.Point point = ee.GetPosition(t);
+                    System.Windows.Point res = new System.Windows.Point(first.X - point.X, first.Y - point.Y);
+                    //обновления местоположения
+                    Canvas.SetLeft(el, -res.X);
+                    Canvas.SetTop(el, -res.Y);
+                }
+            };
+        }
+    }
+}
--- ImageWork/MainWindow.xaml.cs
+++ ImageWork/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,131 @@ namespace ImageWork
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int i1 = 1;//первое изображение
+        public int i2 = 2;//второе изображение
+        public int i3 = 3;//третье изображение
+
         public MainWindow()
         {
             InitializeComponent();
+
+            BtnLeft.Content = @"<";
+            BtnRight.Content = @">";
+
+            //загрузка изображений
+            using (var db = new Try_DB_Entities())
+            {
+                var pictures = db.Picture.ToList();
+                var pic1 = pictures.FirstOrDefault(u => u.Picture_ID == i1);
+                var pic2 = pictures.FirstOrDefault(u => u.Picture_ID == i2);
+                var pic3 = pictures.FirstOrDefault(u => u.Picture_ID == i3);
+
+                Img1.Source = Image_Work.byteToImg(pic1.Picture1);
+                Img2.Source = Image_Work.byteToImg(pic2.Picture1);
+                Img3.Source = Image_Work.byteToImg(pic3.Picture1);
+            }
+        }
+        //кнопка для загрузки данных
+        private void BtnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Multiselect = true;
+            open.Filter = "Изображение *.jpg; *.png; *.jpeg| *.jpg; *.png; *.jpeg";
+            if (open.ShowDialog() == true)
+            {
+                foreach (var item in open.FileNames)
+                {
+                    //загрузка изображений
+                    using (var db = new Try_DB_Entities())
+                    {
+                        byte[] array = Image_Work.ImgToByte(item);
+                        Picture picture = new Picture
+                        {
+                            Picture1 = array
+                        };
+                        db.Picture.Add(picture);
+                        db.SaveChanges();
+                    }
+                }
+            }
+        }
+
+        //скрытие изображения
+        private void largeImg_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            largeImg.Visibility = Visibility.Hidden;
+            gridImg.IsEnabled = true;
+            BtnLoad.IsEnabled = true;
+        }
+
+        private void Img1_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+           System.Windows.Controls.Image img = sender as System.Windows.Controls.Image;
+            largeImg.Visibility = Visibility.Visible;
+            largeImg.Source = img.Source;
+            gridImg.IsEnabled = false;
+            BtnLoad.IsEnabled = false;
+        }
+
+        //перелистывание в левую сторону
+        private void BtnLeft_Click(object sender, RoutedEventArgs e)
+        {
+            //загрузка изображений
+            using (var db = new Try_DB_Entities())
+            {
+                var pictures = db.Picture.ToList();
+                if (i1 == 1)
+                {
+                    i3 = i2;
+                    i2 = i1;
+                    i1 = pictures.LastOrDefault().Picture_ID;
+                }
+                else
+                {
+                    i3 = i2;
+                    i2 = i1;
+                    i1 = i1 - 1;
+                }
+
+                var pic1 = pictures.FirstOrDefault(u => u.Picture_ID == i1);
+                var pic2 = pictures.FirstOrDefault(u => u.Picture_ID == i2);
+                var pic3 = pictures.FirstOrDefault(u => u.Picture_ID == i3);
+
+                Img1.Source = Image_Work.byteToImg(pic1.Picture1);
+                Img2.Source = Image_Work.byteToImg(pic2.Picture1);
+                Img3.Source = Image_Work.byteToImg(pic3.Picture1);
+            }
+        }
+
+        //перелистывание в правую сторону
+        private void BtnRight_Click(object sender, RoutedEventArgs e)
+        {
+            //загрузка изображений
+            using (var db = new Try_DB_Entities())
+            {
+                var pictures = db.Picture.ToList();
+                int iLast = pictures.LastOrDefault().Picture_ID;
+                if (i3 == iLast)
+                {
+                    i1 = i2;
+                    i2 = i3;
+                    i3 = pictures.FirstOrDefault().Picture_ID;
+                }
+                else
+                {
+                    i1 = i2;
+                    i2 = i3;
+                    i3 = i3 + 1;
+                }
+
+                var pic1 = pictures.FirstOrDefault(u => u.Picture_ID == i1);
+                var pic2 = pictures.FirstOrDefault(u => u.Picture_ID == i2);
+                var pic3 = pictures.FirstOrDefault(u => u.Picture_ID == i3);
+
+                Img1.Source = Image_Work.byteToImg(pic1.Picture1);
+                Img2.Source = Image_Work.byteToImg(pic2.Picture1);
+                Img3.Source = Image_Work.byteToImg(pic3.Picture1);
+            }
         }
     }
 }
